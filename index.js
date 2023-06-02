7629e4fc473536f986bc10204e6a97ae0f0d96d4
MehmanLezgin <mcguy250@gmail.com>
2023-06-02 11:10:17 +0400

    .

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Yes yes yes RESPONSE!!!');
+    res.send('Yes yes yes RESPONSE!!! aaa');
 })
 
 app.listen(process.env.PORT, () => {
