a39c6a7ac706812343f037740aa1edaf11560e37
MehmanLezgin <mcguy250@gmail.com>
2023-06-03 08:17:05 +0400

    xx

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const apps = express();
 
 apps.get('/', (req, resu) => {
     const a = 123+567*23.32;
-    resu.send('This is me '+a);
+    resu.send('This is me '+a+' and '+(a/10));
     console.log(111);
     // res.send('This is me');
 })
